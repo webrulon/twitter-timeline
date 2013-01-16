8e38049759299771d5c17ec72bed2df76729698c
Kuldeep Singh Dhaka <kuldeepdhaka9@gmail.com>
2013-01-17 01:49:42 +0530

    redirect problem

--- connect.php
+++ connect.php
@@ -2,11 +2,6 @@
 
 require_once 'init/bootloader.php';
     
-    if(  $twitter->is() ){
-        header('location: ' . TWITTER_LANDPAGE);
-        die();
-    }
-    
     $theme->elem('header');
     $theme->elem('hero-unit');
     $theme->elem('footer');
