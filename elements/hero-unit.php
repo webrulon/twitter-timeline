ffed68dbc66ad40ebdc7fa44746ec12fda99f8fd
Kuldeep Singh Dhaka <kuldeepdhaka9@gmail.com>
2013-01-17 01:52:16 +0530

    url to login_url

--- elements/hero-unit.php
+++ elements/hero-unit.php
@@ -1,6 +1,6 @@
         <div class="hero-unit">
             <h1>Timeline</h1>
             <p>View Your And Your Followers Timeline from twitter</p>
-            <a href="<?php echo $twitter->url() ?>" class="btn btn-primary">Sign In With Twitter</a>
+            <a href="<?php echo $twitter->login_url() ?>" class="btn btn-primary">Sign In With Twitter</a>
         </div><!-- end of .hero-unit -->
         
