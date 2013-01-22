46878df291139b0326dda4d54bf02ee2d1cf7d10
Kuldeep Singh Dhaka <kuldeepdhaka9@gmail.com>
2013-01-23 00:19:40 +0530

    clash of email with url

--- init/func.beautify_tweets.php
+++ init/func.beautify_tweets.php
@@ -1,14 +1,14 @@
 <?php
 
 $twitter_beautify_pattern = array(
-    '/([A-Za-z0-9]*)@([A-Za-z0-9]*)\.([A-Za-z0-9]*)/', //email
+    //'/([A-Za-z0-9]*)@([A-Za-z0-9]*)\.([A-Za-z0-9]*)/', //email
     '/([A-Za-z0-9]*)\:\/\/([A-Za-z0-9]*)\.([A-Za-z0-9]*)\/([A-Za-z0-9]*)/',  // for urls
     '/#([A-Za-z0-9]*)/', //hash
     '/@([A-Za-z0-9]*)/', //screen_name
 );
 
 $twitter_beautify_replace  = array(
-    '<a target="_blank" class="twitter-email" href="mailto:$1@$2.$3">$1@$2.$3</a>',
+    //'<a target="_blank" class="twitter-email" href="mailto:$1@$2.$3">$1@$2.$3</a>',
     '<a target="_blank" class="twitter-link" href="$1://$2.$3/$4">$1://$2.$3/$4</a>',
     '<a target="_blank" class="twitter-hash" href="https://twitter.com/search/?q=%23$1&src=hash">#$1</a>',
     '<a target="_blank" class="twitter-user" href="https://twitter.com/$1">@$1</a>',
